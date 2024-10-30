10d41ded619c6ed2b810c5c605ba1270d0a46b9b
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-10-30 10:04:44 +0800

    feat: route update and auth controller build & methods

--- spatie-permission-demo/routes/web.php
+++ spatie-permission-demo/routes/web.php
@@ -3,6 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
+
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 
 Route::get('/', function () {
     return view('welcome');
