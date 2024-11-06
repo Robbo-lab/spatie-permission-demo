f4d56a95d3a54a42730854cc035edf458d8beedb
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-11-06 09:13:20 +0800

    chore: check

--- spatie-permission-demo/routes/web.php
+++ spatie-permission-demo/routes/web.php
@@ -10,7 +10,6 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-
 Route::get('/', function () {
     return view('welcome');
 });
